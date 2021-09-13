5ecf8fb18d93b5a3b38d11d7dc32f93facc808d7
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2021-09-13 19:04:50 -0300

    Criado Repository e Service para Categoria

---
+++ src/main/java/com/myo/estudoDeCaso/services/CategoriaService.java
@@ -0,0 +1,25 @@
+package com.myo.estudoDeCaso.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.myo.estudoDeCaso.domain.Categoria;
+import com.myo.estudoDeCaso.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		
+		Optional<Categoria> obj = repo.findById(id);
+		
+		return obj.orElse(null);
+		
+	}
+
+}
