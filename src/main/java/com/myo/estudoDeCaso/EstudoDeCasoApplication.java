d37bbf651e9507da6687d22bc5b892578865f139
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2021-09-13 19:21:01 -0300

    Instanciada Categoria

--- src/main/java/com/myo/estudoDeCaso/EstudoDeCasoApplication.java
+++ src/main/java/com/myo/estudoDeCaso/EstudoDeCasoApplication.java
@@ -1,13 +1,32 @@
 package com.myo.estudoDeCaso;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.myo.estudoDeCaso.domain.Categoria;
+import com.myo.estudoDeCaso.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class EstudoDeCasoApplication {
+public class EstudoDeCasoApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstudoDeCasoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
