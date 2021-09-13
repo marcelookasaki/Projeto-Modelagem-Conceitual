49c8976487675eed8dd9b0de3029842bd9dcdba1
Marcelo Yutaka Okasaki <marcelo.okasaki@gmail.com>
2021-09-13 13:26:36 -0300

    Projeto Criado

---
+++ src/main/java/com/myo/estudoDeCaso/EstudoDeCasoApplication.java
@@ -0,0 +1,13 @@
+package com.myo.estudoDeCaso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoDeCasoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoDeCasoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myo/estudoDeCaso/EstudoDeCasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myo.estudoDeCaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudoDeCasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
